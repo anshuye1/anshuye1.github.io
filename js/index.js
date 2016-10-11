caa55fbd9a23df18bec9ab9bc4993450d53c20c5
Your Name <you@example.com>
2016-10-11 23:35:39 +0800

    '3'

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ $(function(){
     var speedY=3;
     var flag=true;
     function lightMove(){
-        if(light.offset().top>($(window).height()-$('.light').width()-10)||light.offset().top<0){
+        if(light.offset().top>($('.light-box').height()-$('.light').width()-10)||light.offset().top<0){
             speedY*=-1;
         }else{
             speedY*=1;
         }
-        if(light.offset().left>($(window).width()-$('.light').width()-10)||light.offset().left<0){
+        if(light.offset().left>($('.light-box').width()-$('.light').width()-10)||light.offset().left<0){
             speedX*=-1;
         }else{
             speedX*=1;
@@ -25,15 +25,16 @@ $(function(){
             clearInterval(t);
             light.addClass('active');
             setTimeout(function(){
-                light.removeClass('active');
-                light.css({
-                    'width':'100%',
-                    'height':'100%',
-                    'border-radius':'0',
-                    'box-sizing':'border-box',
-                    'left':0,
-                    'top':0,
-                    'box-shadow':'0 0 0 0'
+                light.removeClass('active').queue(function(){
+                    light.css({
+                        'width':'100%',
+                        'height':'100%',
+                        'border-radius':'0',
+                        'box-sizing':'border-box',
+                        'left':0,
+                        'top':0,
+                        'box-shadow':'0 0 0 0'
+                    }).dequeue()
                 });
             },3000)
         },9000);
@@ -100,22 +101,24 @@ $(function(){
         $(this).children('a').removeClass('active');
         $(this).children('span').removeClass('active');
     });
-    ////////////////////////////////////////////////////作品
+    ////////////////////////////////////////////////////作品///////////////////////////////////////////////
     var designW=$('.design-inner').width();
     var design=$('.design-inner');
     var index=1;
     var next=1;
-    $('.design-inner:first').css('left',2*designW);
+    $('.design-inner:first').css('left',3*designW);
     $('.design-inner').eq(1).css('left',1*designW);
     $('.design-inner').eq(1).children('a').addClass('active');
     function designMove(){
         next++;
         if(next>design.length-1){next=0;}
-        design.eq(next).css('left',0*designW);
-        design.eq(index).animate({'left':2*designW},1000);
-        design.eq(next).animate({'left':1*designW},1000);
+        design.eq(next).css('left',-3*designW);
+        design.eq(index).animate({'left':3*designW},3000);
+        design.eq(next).animate({'left':1*designW},3000);
         $(design.eq(next).children('a')).addClass('active');
+        $(design.eq(next)).addClass('active');
         $(design.eq(index).children('a')).removeClass('active');
+        $(design.eq(index)).removeClass('active');
         index=next;
     }
     var designT=setInterval(designMove,3000);
@@ -125,26 +128,13 @@ $(function(){
     $('.btn .left').on('click',function(){
         next--;
         if(next<0){next=design.length-1;}
-        design.eq(next).css('left',2*designW);
-        design.eq(index).animate({'left':0*designW},1000);
-        design.eq(next).animate({'left':1*designW},1000);
+        design.eq(next).css('left',3*designW);
+        design.eq(index).animate({'left':-3*designW},3000);
+        design.eq(next).animate({'left':1*designW},3000);
         $(design.eq(next).children('a')).addClass('active');
         $(design.eq(index).children('a')).removeClass('active');
         index=next;
     });
-    // $('.shade-r').on('click',function(){
-    //     $('.shade-r a').addClass('active')
-    // });
-    // $('.shade').on('click',function(){
-    //     next--;
-    //     if(next<0){next=design.length-1;}
-    //     design.eq(next).css('left',2*designW);
-    //     design.eq(index).animate({'left':0*designW},1000);
-    //     design.eq(next).animate({'left':1*designW},1000);
-    //     $(design.eq(next).children('a')).addClass('active');
-    //     $(design.eq(index).children('a')).removeClass('active');
-    //     index=next;
-    // });
     $('.li-box').on('mouseenter',function(){
         $('.btn').addClass('active');
         clearInterval(designT);
@@ -153,11 +143,23 @@ $(function(){
         $('.btn').removeClass('active');
         designT=setInterval(designMove,3000);
     });
-    /////////////////////////////////////////////contact
+    ////////////////////////////////////////////////////作品///////////////////////////////////////////////
+    $('.game-box span').on('click',function(){
+        $('.game-box span').removeClass('active');
+        $(this).addClass('active');
+        $('.shade li').removeClass('active');
+        $('.shade li').eq($(this).index()).addClass('active');
+        if($(this).index()!=0){
+            $('.shade').removeClass('active');
+        }else{
+            $('.shade').addClass('active');
+        }
+    });
+    /////////////////////////////////////////////contact////////////////////////////////////////////////////////
     $('.callBtn').on('click',function(){
         $('.call a').toggleClass('active');
     });
-    ////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
     var H=$('body').scrollTop();
     console.log(H);
     if(H>660){
